2c00b9329626766e8489c11e2ec89775246d0466
Łukasz Zachodni <lukasz.zachodni@billennium.com>
2022-07-22 12:39:39 +0200

    swagger3

--- src/main/java/com/billennium/crud/CrudApplication.java
+++ src/main/java/com/billennium/crud/CrudApplication.java
@@ -1,15 +1,17 @@
 package com.billennium.crud;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @ComponentScan({"com.billennium.crud"})
 @EntityScan({"com.billennium.crud"})
+@SecurityScheme(name = "crud_api", scheme = "JWT", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER)
 public class CrudApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/billennium/crud/api/BookRestController.java
+++ src/main/java/com/billennium/crud/api/BookRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.Optional;
 
-@RestController
+@RestController("/api")
 @AllArgsConstructor
 @RequestMapping("/api")
 public class BookRestController {
